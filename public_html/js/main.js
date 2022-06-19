b0e5d037afd1a00199ec579009f4a64f3dabef6f
ahmar7 <ahmarjabbar7@gmail.com>
2022-06-19 18:41:31 +0500

    roofing site

---
+++ public_html/js/main.js
@@ -0,0 +1,10 @@
+    //slide
+
+    jQuery('.autoplay').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        arrows: false,
+        dots: true,
+    });
